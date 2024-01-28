a4245b49227d1775adddc3f5002fd84ff80230cc
Ramamia <130895629+Ramamia@users.noreply.github.com>
2024-01-28 22:39:08 +0300

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = new mysqli("localhost","root","","happycapy") ; 
+/* $con - new mysqli("if0_35859917_happycapy","if0_35859917","IvCuUBmyJP1zYg","sql203.infinityfree.com	
+ ");*/
+
+/* if($con) {
+    echo "success connection";
+} else {
+    die(mysqli_error($con));
+} */
+
+?>
